edfae18b4f33e5eaf372c68d1dbfa593cd091cb9
nekkox <klascibox@gmail.com>
2024-06-22 17:12:44 +0200

    Parting Admin Dashboard

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -11,7 +11,7 @@ class AdminDashboardController extends Controller
 {
     public function index() : View {
 
-        //return view('admin.dashboard.index');
-        return view('admin.layouts.master');
+        return view('admin.dashboard.index');
+       //return view('admin.layouts.master');
     }
 }
