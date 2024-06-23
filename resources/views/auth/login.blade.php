8a6004478f9ba15393a32518552487785bad8f60
nekkox <klascibox@gmail.com>
2024-06-23 12:25:47 +0200

    Create LOGIN page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
-<x-guest-layout>
+{{--<x-guest-layout>
+
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +45,103 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout>--}}
+@extends('frontend.layouts.master')
+@section('content')
+    <!--=============================
+        BREADCRUMB START
+    ==============================-->
+    <section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>sign in</h1>
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="#">sign in</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=============================
+        BREADCRUMB END
+    ==============================-->
+
+
+    <!--=========================
+        SIGNIN START
+    ==========================-->
+    <section class="fp__signin" style="background: url(images/login_bg.jpg);">
+        <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class="container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area">
+                            <h2>Welcome back!</h2>
+                            <p>sign in to continue</p>
+
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <!-- Email Address -->
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="email">email</label>
+                                            <input type="email" id="email" name="email" placeholder="Email"
+                                                   value="{{old('email')}}" required autofocus autocomplete="username">
+                                        </div>
+                                    </div>
+
+
+                                    <!-- Password -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="password">password</label>
+                                            <input id="password" name="password" type="password" placeholder="Password"
+                                                   required autocomplete="current-password">
+                                        </div>
+                                    </div>
+
+
+                                    <!-- Remember Me -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput fp__login_check_area">
+                                            <div class="form-check">
+
+                                                <label class="form-check-label" for="remember_me">
+                                                    <input class="form-check-input" type="checkbox"
+                                                           id="remember_me" name="remember" value="" style="margin-left: 10px"> Remember Me
+                                                </label>
+                                            </div>
+                                            <a href="forgot_password.html">Forgot Password ?</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="or"><span>or</span></p>
+                            <ul class="d-flex">
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                            </ul>
+                            <p class="create_account">Dont’t have an aceount ? <a href="sign_up.html">Create Account</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=========================
+        SIGNIN END
+    ==========================-->
+@endsection
+
