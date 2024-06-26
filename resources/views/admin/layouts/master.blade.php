57dc07b3ed608826e85d1e9aca4f390745df1e04
nekkox <klascibox@gmail.com>
2024-06-26 16:22:13 +0200

    Install Yajrabox Datatables

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -13,7 +13,9 @@
     <!-- Template CSS -->
     <link rel="stylesheet" href={{asset("admin/assets/css/style.css")}}>
     <link rel="stylesheet" href={{asset("admin/assets/css/components.css")}}>
+
     <link rel="stylesheet" href="{{asset('admin/assets/css/toastr.min.css')}}">
+    <link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
     <!-- Start GA -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
     <script>
@@ -62,6 +64,7 @@
 <script src={{asset("admin/assets/js/stisla.js")}}></script>
 
 <script src="{{asset('admin/assets/js/toastr.min.js')}}"></script>
+<script src="//cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
 <!-- Template JS File -->
 <script src={{asset("admin/assets/js/scripts.js")}}></script>
 <script src={{asset("admin/assets/js/custom.js")}}></script>
