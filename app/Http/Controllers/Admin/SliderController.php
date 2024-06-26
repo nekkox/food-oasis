ce7f04aeacf0cd32832a22be49d384cd207ebe54
nekkox <klascibox@gmail.com>
2024-06-26 21:50:42 +0200

    Slider STORE route and storing data in db

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\SliderDataTable;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\SliderCreateRequest;
+use App\Models\Slider;
+use App\Traits\FileUploadTrait;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class SliderController extends Controller
 {
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -31,7 +34,21 @@ class SliderController extends Controller
      */
     public function store(SliderCreateRequest $request)
     {
-        //$request->validate();
+       /* Handle image upload */
+        $imagePath = $this->uploadImage($request, 'image');
+        $slider = new Slider();
+        $slider->image = $imagePath;
+        $slider->offer = $request->offer;
+        $slider->title = $request->title;
+        $slider->sub_title = $request->sub_title;
+        $slider->short_description = $request->short_description;
+        $slider->button_link = $request->button_link;
+        $slider->status = $request->status;
+        $slider->save();
+
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.slider.index');
     }
 
     /**
