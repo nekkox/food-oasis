c1073d46226b028123b9ab640caf7db2d74c5479
nekkox <klascibox@gmail.com>
2024-06-26 14:26:49 +0200

    Create Slider controller + resource routes

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SliderController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,6 +16,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('profile', [ProfileController::class, 'index'])->name('profile');
     Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
     Route::put('profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+
+    /*Slider Routes*/
+    Route::get('slider',[SliderController::class, 'index'])->name('slider.index');
 
 });
 
