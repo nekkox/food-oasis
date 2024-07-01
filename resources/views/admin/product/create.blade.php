4328919ad0d03c0ba83b0024262be1791084e8d9
nekkox <klascibox@gmail.com>
2024-07-01 18:52:46 +0200

    create Product ProductCreateRequest

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -14,7 +14,7 @@
 
             <div class="card-body">
 
-                <form action="{{route('admin.category.store')}}" method="post" enctype="multipart/form-data">
+                <form action="{{route('admin.product.store')}}" method="post" enctype="multipart/form-data">
                     @csrf
 
                     <div class="form-group">
