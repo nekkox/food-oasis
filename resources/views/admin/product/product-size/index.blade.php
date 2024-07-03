d6e99f83bf40ddd68a54f98f3b73b4216d084275
nekkox <klascibox@gmail.com>
2024-07-03 14:27:07 +0200

    Create Product-options

--- resources/views/admin/product/product-size/index.blade.php
+++ resources/views/admin/product/product-size/index.blade.php
@@ -11,73 +11,145 @@
             <a href="{{route('admin.product.index')}}" class="btn btn-primary my-3">Go Back</a>
         </div>
 
-        <div class="card card-primary">
-            <div class="card-header">
-                <h4>Create Size</h4>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h4>Create Size</h4>
+                    </div>
+
+                    <div class="card-body">
+
+                        <form action="{{route('admin.product-size.store')}}" method="post" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="name"  class="form-label">Name: </label>
+                                        <input type="text" id="name" name="name" class="form-control">
+                                        <input type="hidden" name="product_id" value="{{ $product->id}}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="price" class="form-label">Price: </label>
+                                        <input type="text" id="price" name="price" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+
+                <div class="card card-primary">
+
+                    <div class="card-body">
+                        <table class="table table-bordered ">
+                            <thead>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                            </thead>
+                            <tbody>
+                            @foreach($sizes as $size)
+                                <tr class="container list-group-item-action item">
+                                    <td>{{$size->name}}</td>
+                                    <td>{{$size->price}}</td>
+                                    <td style="position: relative;">
+                                        <a style="position: absolute; right: 20px; bottom: 15px"
+                                           href='{{route('admin.product-size.destroy', $size->id)}}'
+                                           class='btn btn-danger delete-item'><i class='fas fa-trash'></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            @if(count($sizes) === 0)
+                                <tr>
+                                    <td class="text-center" colspan="3">No Data Found</td>
+                                </tr>
+                            @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
+            <div class="col-md-6">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h4>Create Product Options</h4>
+                    </div>
 
-            <div class="card-body">
+                    <div class="card-body">
 
-                    <form action="{{route('admin.product-size.store')}}" method="post" enctype="multipart/form-data">
-                        @csrf
+                        <form action="{{route('admin.product-option.store')}}" method="post" enctype="multipart/form-data">
+                            @csrf
 
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="name"  class="form-label">Name: </label>
-                                    <input type="text" id="name" name="name" class="form-control">
-                                    <input type="hidden" name="product_id" value="{{ $product->id}}">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="name"  class="form-label">Name: </label>
+                                        <input type="text" id="name" name="name" class="form-control">
+                                        <input type="hidden" name="product_id" value="{{ $product->id}}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="price" class="form-label">Price: </label>
+                                        <input type="text" id="price" name="price" class="form-control">
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="price" class="form-label">Price: </label>
-                                    <input type="text" id="price" name="price" class="form-control">
-                                </div>
+
+
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Create</button>
                             </div>
-                        </div>
+                        </form>
 
+                    </div>
+                </div>
 
+                <div class="card card-primary">
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Create</button>
-                        </div>
-                    </form>
-
+                    <div class="card-body">
+                        <table class="table table-bordered ">
+                            <thead>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                            </thead>
+                            <tbody>
+                            @foreach($sizes as $size)
+                                <tr class="container list-group-item-action item">
+                                    <td>{{$size->name}}</td>
+                                    <td>{{$size->price}}</td>
+                                    <td style="position: relative;">
+                                        <a style="position: absolute; right: 20px; bottom: 15px"
+                                           href='{{route('admin.product-size.destroy', $size->id)}}'
+                                           class='btn btn-danger delete-item'><i class='fas fa-trash'></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            @if(count($sizes) === 0)
+                                <tr>
+                                    <td class="text-center" colspan="3">No Data Found</td>
+                                </tr>
+                            @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
 
-
-        <div class="card card-primary">
-
-            <div class="card-body">
-                <table class="table table-bordered ">
-                    <thead>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Action</th>
-                    </thead>
-                    <tbody>
-                    @foreach($sizes as $size)
-                        <tr class="container list-group-item-action item">
-                            <td>{{$size->name}}</td>
-                            <td>{{$size->price}}</td>
-                            <td style="position: relative;">
-                                <a style="position: absolute; right: 20px; bottom: 15px"
-                                   href='{{route('admin.product-size.destroy', $size->id)}}'
-                                   class='btn btn-danger delete-item'><i class='fas fa-trash'></i></a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    @if(count($sizes) === 0)
-                        <tr>
-                            <td class="text-center" colspan="3">No Data Found</td>
-                        </tr>
-                    @endif
-                    </tbody>
-                </table>
-            </div>
-        </div>
 
     </section>
 @endsection
