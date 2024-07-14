6e0eae055c9bf45377926bd9d5b991ac46dc0f5e
nekkox <klascibox@gmail.com>
2024-07-14 17:45:00 +0200

    Address edit feature, frontend

--- resources/views/frontend/dashboard/sections/address-section.blade.php
+++ resources/views/frontend/dashboard/sections/address-section.blade.php
@@ -1,44 +1,42 @@
-<div class="tab-pane fade" id="v-pills-address" role="tabpanel"
-     aria-labelledby="v-pills-address-tab">
+<div class="tab-pane fade" id="v-pills-address" role="tabpanel" aria-labelledby="v-pills-address-tab">
     <div class="fp_dashboard_body address_body">
         <h3>address <a class="dash_add_new_address"><i class="far fa-plus"></i> add new
             </a>
         </h3>
-        <div class="fp_dashboard_address">
+        <div class="fp_dashboard_address show_edit_address">
             <div class="fp_dashboard_existing_address">
                 <div class="row">
-
-                    @foreach($userAddresses as $address)
+                    @foreach ($userAddresses as $address)
                         <div class="col-md-6">
                             <div class="fp__checkout_single_address">
                                 <div class="form-check">
                                     <label class="form-check-label">
-                                    <span class="icon">
-                                        @if ( $address->type === 'home')
-                                            <i class="fas fa-home"></i>
-                                        @else
-                                            <i class="far fa-car-building"></i>
-                                        @endif
-                                        {{ $address->type }}</span>
-                                        <span class="address">{{ $address->address }}, {{ $address->deliveryArea?->area_name }} </span>
+                                        <span class="icon">
+                                            @if ($address->type === 'home')
+                                                <i class="fas fa-home"></i>
+                                            @else
+                                                <i class="far fa-car-building"></i>
+                                            @endif
+
+                                            {{ $address->type }}
+                                        </span>
+                                        <span class="address">{{ $address->address }},
+                                            {{ $address->deliveryArea?->area_name }} </span>
                                     </label>
                                 </div>
                                 <ul>
-                                    <li><a class="dash_edit_btn"><i
-                                                class="far fa-edit"></i></a></li>
-                                    <li><a class="dash_del_icon"><i
-                                                class="fas fa-trash-alt"></i></a>
+                                    <li><a class="dash_edit_btn show_edit_section" data-class="edit_section_{{ $address->id }}"><i class="far fa-edit"></i></a></li>
+                                    <li><a class="dash_del_icon"><i class="fas fa-trash-alt"></i></a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     @endforeach
+
                 </div>
             </div>
-
-            {{--ADD NEW ADDRESS--}}
             <div class="fp_dashboard_new_address ">
-                <form action="{{route('address.store')}}" method="POST">
+                <form action="{{ route('address.store') }}" method="POST">
                     @csrf
                     <div class="row">
                         <div class="col-12">
@@ -47,13 +45,10 @@
 
                         <div class="col-md-12 col-lg-12 col-xl-12">
                             <div class="fp__check_single_form">
-
-
                                 <select id="select_js3" name="area">
-                                    <option value="">select area</option>
-                                    @foreach($deliveryAreas as $area)
-                                        <option value="{{$area->id}}">{{$area->area_name}}</option>
-
+                                    <option value="">Select Area</option>
+                                    @foreach ($deliveryAreas as $area)
+                                        <option  value="{{ $area->id }}">{{ $area->area_name }}</option>
                                     @endforeach
 
                                 </select>
@@ -82,7 +77,6 @@
                             </div>
                         </div>
 
-
                         <div class="col-md-12 col-lg-12 col-xl-12">
                             <div class="fp__check_single_form">
                                 <textarea cols="3" rows="4" placeholder="Address" name="address"></textarea>
@@ -91,148 +85,124 @@
                         <div class="col-12">
                             <div class="fp__check_single_form check_area">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio"
-                                           name="type"
-                                           id="flexRadioDefault1" value="home">
-                                    <label class="form-check-label"
-                                           for="flexRadioDefault1">
+                                    <input class="form-check-input" type="radio" name="type" id="flexRadioDefault1"
+                                           value="home">
+                                    <label class="form-check-label" for="flexRadioDefault1">
                                         home
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio"
-                                           name="type"
-                                           id="flexRadioDefault2" value="office">
-                                    <label class="form-check-label"
-                                           for="flexRadioDefault2">
+                                    <input class="form-check-input" type="radio" name="type" id="flexRadioDefault2"
+                                           value="office">
+                                    <label class="form-check-label" for="flexRadioDefault2">
                                         office
                                     </label>
                                 </div>
                             </div>
                         </div>
                         <div class="col-12">
-                            <button type="button"
-                                    class="common_btn cancel_new_address">cancel
-                            </button>
+                            <button type="button" class="common_btn cancel_new_address">cancel</button>
                             <button type="submit" class="common_btn">save
-                                address
-                            </button>
+                                address</button>
                         </div>
                     </div>
                 </form>
             </div>
-            <div class="fp_dashboard_edit_address ">
-                <form>
-                    <div class="row">
-                        <div class="col-12">
-                            <h4>edit address </h4>
-                        </div>
+            @foreach ($userAddresses as $address)
+                <div class="fp_dashboard_edit_address edit_section_{{ $address->id }}" >
+                    <form action="" method="Post">
+                        @csrf
+                        @method('PUT')
+                        <div class="row">
+                            <div class="col-12">
+                                <h4>Edit Address</h4>
+                            </div>
 
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="First Name">
-                            </div>
-                        </div>
+                            <div class="col-md-12 col-lg-12 col-xl-12">
+                                <div class="fp__check_single_form">
+                                    <select id="select_js4" name="area">
+                                        <option value="">Select Area</option>
+                                        @foreach ($deliveryAreas as $area)
+                                            <option @selected($address->delivery_area_id === $area->id) value="{{ $area->id }}">{{ $area->area_name }}</option>
+                                        @endforeach
 
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Last Name">
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-lg-12 col-xl-12">
-                            <div class="fp__check_single_form">
-                                <input type="text"
-                                       placeholder="Company Name (Optional)">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <select id="select_js4">
-                                    <option value="">select country</option>
-                                    <option value="">bangladesh</option>
-                                    <option value="">nepal</option>
-                                    <option value="">japan</option>
-                                    <option value="">korea</option>
-                                    <option value="">thailand</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Street Address *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text"
-                                       placeholder="Apartment, suite, unit, etc. (optional)">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Town / City *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="State *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Zip *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Phone *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="email" placeholder="Email *">
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-lg-12 col-xl-12">
-                            <div class="fp__check_single_form">
-                                                                <textarea cols="3" rows="4"
-                                                                          placeholder="Address"></textarea>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="fp__check_single_form check_area">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio"
-                                           name="flexRadioDefault2"
-                                           id="flexRadioDefault12">
-                                    <label class="form-check-label"
-                                           for="flexRadioDefault12">
-                                        home
-                                    </label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio"
-                                           name="flexRadioDefault2"
-                                           id="flexRadioDefault22">
-                                    <label class="form-check-label"
-                                           for="flexRadioDefault22">
-                                        office
-                                    </label>
+                                    </select>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-12">
-                            <button type="button"
-                                    class="common_btn cancel_edit_address">cancel
-                            </button>
 
-                            <button type="submit" class="common_btn">update
-                                address
-                            </button>
+                            <div class="col-md-6 col-lg-12 col-xl-6">
+                                <div class="fp__check_single_form">
+                                    <input type="text" placeholder="First Name" name="first_name" value="{{ $address->first_name }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-12 col-xl-6">
+                                <div class="fp__check_single_form">
+                                    <input type="text" placeholder="Last Name" name="last_name"  value="{{ $address->last_name }}">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 col-lg-12 col-xl-6">
+                                <div class="fp__check_single_form">
+                                    <input type="text" placeholder="Phone" name="phone" value="{{ $address->phone }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-12 col-xl-6">
+                                <div class="fp__check_single_form">
+                                    <input type="text" placeholder="Email" name="email" value="{{ $address->email }}">
+                                </div>
+                            </div>
+
+                            <div class="col-md-12 col-lg-12 col-xl-12">
+                                <div class="fp__check_single_form">
+                                    <textarea cols="3" rows="4" placeholder="Address" name="address">{{$address->address}}</textarea>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="fp__check_single_form check_area">
+                                    <div class="form-check">
+                                        <input @checked($address->type === 'home') class="form-check-input" type="radio" name="type"
+                                               id="flexRadioDefault1" value="home">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            home
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input @checked($address->type === 'office') class="form-check-input" type="radio" name="type"
+                                               id="flexRadioDefault2" value="office">
+                                        <label class="form-check-label" for="flexRadioDefault2">
+                                            office
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button type="button" class="common_btn cancel_edit_address">cancel</button>
+                                <button type="submit" class="common_btn">save
+                                    address</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
+
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.show_edit_section').on('click', function(){
+                let className = $(this).data('class');
+                $('.fp_dashboard_edit_address').removeClass('d-block').removeClass('d-none')
+               // $('.fp_dashboard_edit_address').removeClass('d-none')
+                $('.fp_dashboard_existing_address').addClass('d-none');
+                $('.'+className).addClass('d-block');
+            })
+
+            $('.cancel_edit_address').on('click', function(){
+               $('.fp_dashboard_edit_address').addClass('d-none')
+                $('.fp_dashboard_existing_address').removeClass('d-none');
+            })
+        })
+    </script>
+@endpush
