32cc8192c0d3183b851f97af957a8add5f9a5e4e
nekkox <klascibox@gmail.com>
2024-07-27 20:03:53 +0200

    Chefs Section - CREATE action

--- app/Http/Controllers/Admin/ChefController.php
+++ app/Http/Controllers/Admin/ChefController.php
@@ -2,39 +2,66 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DataTables\ChefDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ChefCreateRequest;
+use App\Models\Chef;
+use App\Traits\FileUploadTrait;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChefController extends Controller
 {
+
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(ChefDataTable $dataTable)
     {
-        //
+        return $dataTable->render('admin.chef.index');
+
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        return view('admin.chef.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ChefCreateRequest  $request)
     {
-        //
+        $imagePath = $this->uploadImage($request, 'image');
+
+        $chef = new Chef();
+        $chef->image = $imagePath;
+        $chef->name = $request->name;
+        $chef->title = $request->title;
+        $chef->fb = $request->fb;
+        $chef->in = $request->in;
+        $chef->x = $request->x;
+        $chef->web = $request->web;
+        $chef->show_at_home = $request->show_at_home;
+        $chef->status = $request->status;
+        $chef->save();
+
+       // toastr()->success('Created Successfully!');
+
+        return to_route('admin.chefs.index')->with('created', true);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id) : RedirectResponse
     {
         //
     }
@@ -42,7 +69,7 @@ class ChefController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
         //
     }
@@ -50,7 +77,7 @@ class ChefController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id) : RedirectResponse
     {
         //
     }
@@ -58,7 +85,7 @@ class ChefController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id) : Response
     {
         //
     }
