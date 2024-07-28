ba98be9c6259efce39e568e1db8c0d763778d593
nekkox <klascibox@gmail.com>
2024-07-28 14:31:49 +0200

    App Banner section - Update form

--- resources/views/admin/app-download-section/index.blade.php
+++ resources/views/admin/app-download-section/index.blade.php
@@ -14,6 +14,10 @@
             <div class="card-body">
                 <form action="{{ route('admin.banner-slider.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
+
+
+                    <div class="row">
+                        <div class="col-md-3">
                     <div class="form-group">
                         <label>Image</label>
                         <div id="image-preview" class="image-preview">
@@ -21,12 +25,42 @@
                             <input type="file" name="image" id="image-upload" />
                         </div>
                     </div>
+                    </div>
+
+
+                        <div class="col-md-3">
+                    <div class="form-group">
+                        <label>Background</label>
+                        <div id="image-preview-2" class="image-preview">
+                            <label for="image-upload" id="image-label-2">Choose File</label>
+                            <input type="file" name="image" id="image-upload-2" />
+                        </div>
+                    </div>
+                        </div>
+
+                    </div>
+
+
 
                     <div class="form-group">
                         <label for="">Title</label>
                         <input type="text" class="form-control" name="title">
                     </div>
 
+                    <div class="form-group">
+                        <label for="">Description</label>
+                        <textarea name="short_description" id="" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Play Store Link <code>(Leave empty for hide)</code></label>
+                        <input type="text" class="form-control" name="title">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Apple Store Link <code>(Leave empty for hide)</code></label>
+                        <input type="text" class="form-control" name="title">
+                    </div>
 
                     <button type="submit" class="btn btn-primary">Create</button>
                 </form>
@@ -34,3 +68,26 @@
         </div>
     </section>
 @endsection
+
+@push('scripts')
+    <script>
+        $.uploadPreview({
+            input_field: "#image-upload", // Default: .image-upload
+            preview_box: "#image-preview", // Default: .image-preview
+            label_field: "#image-label", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+        $.uploadPreview({
+            input_field: "#image-upload-2", // Default: .image-upload
+            preview_box: "#image-preview-2", // Default: .image-preview
+            label_field: "#image-label-2", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+    </script>
+@endpush
