102203bcded19d14fc2d0ec65a46ef69550e9719
nekkox <klascibox@gmail.com>
2024-07-28 20:32:06 +0200

    Testimonial Section - dynamic titles in admin dashboard

--- app/Http/Controllers/Admin/TestimonialController.php
+++ app/Http/Controllers/Admin/TestimonialController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\TestimonialDataTable;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\TestimonialCreateRequest;
+use App\Http\Requests\Admin\TestimonialUpdateRequest;
+use App\Models\SectionTitle;
 use App\Models\Testimonial;
 use App\Traits\FileUploadTrait;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TestimonialController extends Controller
 {
@@ -18,7 +22,10 @@ class TestimonialController extends Controller
      */
     public function index(TestimonialDataTable $dataTable)
     {
-        return $dataTable->render('admin.testimonial.index');
+        $keys = ['testimonial_top_title', 'testimonial_main_title', 'testimonial_sub_title'];
+        $titles = SectionTitle::whereIn('key', $keys)->pluck('value','key');
+
+        return $dataTable->render('admin.testimonial.index',['titles' => $titles]);
     }
 
     /**
@@ -32,7 +39,7 @@ class TestimonialController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(TestimonialCreateRequest $request)
+    public function store(TestimonialCreateRequest $request) : RedirectResponse
     {
         $imagePath = $this->uploadImage($request, 'image');
 
@@ -64,22 +71,63 @@ class TestimonialController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $testimonial = Testimonial::findOrFail($id);
+        return view('admin.testimonial.edit', ['testimonial'=>$testimonial]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TestimonialUpdateRequest $request, string $id)
     {
-        //
+        $imagePath = $this->uploadImage($request, 'image', $request->old_image);
+
+        $testimonial = Testimonial::findOrFail($id);
+        $testimonial->image = !empty($imagePath) ? $imagePath : $request->old_image;
+        $testimonial->name = $request->name;
+        $testimonial->title = $request->title;
+        $testimonial->rating = $request->rating;
+        $testimonial->review = $request->review;
+        $testimonial->show_at_home = $request->show_at_home;
+        $testimonial->status = $request->status;
+        $testimonial->save();
+
+        //toastr()->success('Created Successfully');
+
+        return to_route('admin.testimonial.index')->with('updated', true);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id) : Response
     {
         //
+        try {
+            $testimonial = Testimonial::findOrFail($id);
+            $this->removeImage($testimonial->image);
+            $testimonial->delete();
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        } catch (\Exception $e) {
+            return response(['status' => 'error', 'message' => 'something went wrong!']);
+        }
     }
+
+    public function updateTitle(Request $request): RedirectResponse
+    {
+        $validatedData = $request->validate([
+            'testimonial_top_title' => ['max:100'],
+            'testimonial_main_title' => ['max:200'],
+            'testimonial_sub_title' => ['max:500']
+        ]);
+        foreach ($validatedData as $key => $value) {
+            SectionTitle::updateOrCreate(
+                ['key' => $key],
+                ['value' => $value]
+            );
+        }
+        //toastr()->success('Updated Successfully!');
+        return redirect()->back()->with('updated', true);
+    }
+
 }
