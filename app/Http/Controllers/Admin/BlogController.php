755a54b6ba9af8c957bc66d811549d5ac619f085
nekkox <klascibox@gmail.com>
2024-07-29 16:13:59 +0200

    Blog Section - Blog - CREATE action

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\BlogCategoryDataTable;
 use App\DataTables\BlogDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\BlogCreateRequest;
+use App\Models\Blog;
+use App\Models\BlogCategory;
+use App\Traits\FileUploadTrait;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -24,15 +30,32 @@ class BlogController extends Controller
      */
     public function create()
     {
-        //
+        $categories = BlogCategory::all();
+        return view('admin.blog.create',['categories'=>$categories]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BlogCreateRequest $request)
     {
-        //
+        $imagePath = $this->uploadImage($request, 'image');
+
+        $blog = new Blog();
+        $blog->user_id = auth()->user()->id;
+        $blog->image = $imagePath;
+        $blog->title = $request->title;
+        $blog->slug = Str::slug($request->title);
+        $blog->category_id = $request->category;
+        $blog->description = $request->description;
+        $blog->seo_title = $request->seo_title;
+        $blog->seo_description = $request->seo_description;
+        $blog->status = $request->status;
+        $blog->save();
+
+       // toastr()->success('Created Successfully');
+
+        return to_route('admin.blogs.index')->with('created', true);
     }
 
     /**
