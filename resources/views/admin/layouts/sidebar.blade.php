e5b7cd5e713bf7257534b08cef45a8de2dd8868e
nekkox <klascibox@gmail.com>
2024-07-29 12:09:26 +0200

    Blog Section - Blog Category - creating necessary files and designs

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -104,6 +104,15 @@
 
 
             <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Blog</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{route('admin.blog-category.index')}}">Categories</a></li>
+                    <li><a class="nav-link" href="{{ route('admin.delivery-area.index') }}">Blog</a></li>
+                </ul>
+            </li>
+
+
+            <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
                         class="fas fa-columns"></i>
                     <span>Sections </span></a>
--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\AppDownloadSectionController;
 use App\Http\Controllers\Admin\BannerSliderController;
+use App\Http\Controllers\Admin\BlogCategoryController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ChatController;
 use App\Http\Controllers\Admin\ChefController;
@@ -130,6 +131,10 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     /** Counter Routes */
     Route::get('counter', [CounterController::class, 'index'])->name('counter.index');
     Route::put('counter', [CounterController::class, 'update'])->name('counter.update');
+
+
+    /** Blogs Routes */
+    Route::resource('blog-category', BlogCategoryController::class);
 
 });
 
