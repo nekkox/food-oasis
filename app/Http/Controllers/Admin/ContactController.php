375a294feb4374c27ae6127d40bbd0cc4a5c3295
nekkox <klascibox@gmail.com>
2024-07-31 11:29:16 +0200

    Contact page - upsert form in admin dashboard

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ContactUpdateRequest;
+use App\Models\Contact;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public  function index(){
-        return view('admin.contact.index');
+        $contact = Contact::first();
+        return view('admin.contact.index',['contact' => $contact]);
+    }
+
+
+    function update(ContactUpdateRequest $request): RedirectResponse
+    {
+        Contact::updateOrCreate(
+            ['id' => 1],
+            [
+                'phone_one' => $request->phone_one,
+                'phone_two' => $request->phone_two,
+                'mail_one' => $request->mail_one,
+                'mail_two' => $request->mail_two,
+                'address' => $request->address,
+                'map_link' => $request->map_link
+            ]
+        );
+
+       // toastr()->success('Created Successfully');
+
+        return redirect()->back()->with('updated', true);
     }
 }
