2a792e7c17a7de4a8f6da8ad83b085a2e5549891
nekkox <klascibox@gmail.com>
2024-08-04 22:41:52 +0200

    Rating Product Feature - post form subbmiting ratking and review

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
         return $this->hasMany(Chat::class, 'sender_id', 'id')
             ->orWhere('receiver_id', $this->id);
     }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class,'user_id', 'id' );
+    }
 }
